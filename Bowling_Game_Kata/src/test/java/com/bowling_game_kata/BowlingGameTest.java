425a33598c468c683349ef33fddd6fe7f65aed25
Jan Godziatkowski <ph.godzio@gmail.com>
2015-04-23 20:45:01 +0200

    Test 1 Red

--- Bowling_Game_Kata/src/test/java/com/bowling_game_kata/BowlingGameTest.java
+++ Bowling_Game_Kata/src/test/java/com/bowling_game_kata/BowlingGameTest.java
@@ -5,10 +5,23 @@
  */
 package com.bowling_game_kata;
 
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 /**
  *
  * @author Godzio
  */
 public class BowlingGameTest {
-    
+
+    @Test
+    public void testGutterGame() throws Exception {
+        Game game = new Game();
+        for ( int i = 0; i < 20; i++ ) {
+            game.roll( 0 );
+        }
+        assertThat( game.score(), is( 0 ) );
+    }
 }
